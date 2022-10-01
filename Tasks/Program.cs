0eb2e7e0662284e317d5b324b7bf9f549f9f93e6
Roman <roman_strashko.8@mail.ru>
2022-10-01 12:09:04 +0300

    Решение задач

---
+++ Tasks/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 1. Задайте массив заполненый случайными положительными трехзначными числами. Напишите программу, которая покажет количество четных чисел в массиве.
+
+int[] array = GetArray(8, 100, 999);
+Console.WriteLine($"[{String.Join(", ", array)}] -> Количество четных чисел {Coun()}");
+
+int Coun()
+{
+    int count = 0;
+ for(int i = 0; i < array.Length; i++)
+{
+    if(array[i] % 2 == 0)
+    count++;
+}
+return count;
+}
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
